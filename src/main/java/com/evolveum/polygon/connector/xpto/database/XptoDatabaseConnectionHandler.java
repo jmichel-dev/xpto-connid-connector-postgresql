4d17f04686f77f0c175c3e4f477f9ef2dadddd9c
Jean Michel <jean.michel@ebz.tec.br>
2023-02-03 15:59:14 -0300

    database connection handle

--- src/main/java/com/evolveum/polygon/connector/xpto/database/XptoDatabaseConnectionHandler.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/database/XptoDatabaseConnectionHandler.java
@@ -1,2 +1,36 @@
-package com.evolveum.polygon.connector.xpto.database;public class XptoDatabaseConnectionHandler {
+package com.evolveum.polygon.connector.xpto.database;
+
+import com.evolveum.polygon.connector.xpto.XptoConfiguration;
+import com.evolveum.polygon.connector.xpto.utils.StringAccessor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class XptoDatabaseConnectionHandler {
+    private XptoConfiguration configuration;
+
+    public XptoDatabaseConnectionHandler(XptoConfiguration configuration) { this.configuration = configuration; }
+
+    protected Connection connect() throws SQLException {
+        String uri = "jdbc:postgresql://" +
+                configuration.getDatabaseHost() +
+                ":" + configuration.getDatabasePort() +
+                "/" + configuration.getDatabaseName();
+
+        StringAccessor accessor = new StringAccessor();
+
+        Properties props = new Properties();
+        props.setProperty("user", configuration.getDatabaseUser());
+        
+        this.configuration.getDatabasePassword().access(accessor);
+        props.setProperty("password", accessor.getValue());
+        
+        return DriverManager.getConnection(uri, props);
+    }
+
+    protected void setConfiguration(XptoConfiguration configuration) {
+        this.configuration = configuration;
+    }
 }

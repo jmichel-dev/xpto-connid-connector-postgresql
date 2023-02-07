906ae85fcfa25f68ed36ee180343fc256725a77e
Jean Michel <jean.michel@ebz.tec.br>
2023-02-07 10:06:47 -0300

    defined test for test connection

--- src/test/java/com/evolveum/polygon/connector/xpto/config/TestOpTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/TestOpTest.java
@@ -1,4 +1,28 @@
 package com.evolveum.polygon.connector.xpto.config;
 
-public class TestOpTest {
+import com.evolveum.polygon.connector.xpto.XptoConfiguration;
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.exceptions.ConnectionFailedException;
+import org.testng.annotations.Test;
+
+public class TestOpTest extends BaseConnectionTest{
+
+    @Test
+    public void shouldBeAbleToConnectToTheDatabase() throws Exception {
+        ConnectorFacade connector = setupConnector();
+        connector.test();
+    }
+    @Test(expectedExceptions = ConnectionFailedException.class)
+    public void shouldNotBeAbleToConnectToTheDatabaseWithWrongPassword() {
+        XptoConfiguration config = new XptoConfiguration();
+        config.setDatabaseHost("localhost");
+        config.setDatabasePort("5432");
+        config.setDatabaseName("salutaris");
+        config.setDatabaseUser("salutaris");
+        config.setDatabasePassword(new GuardedString("wrong".toCharArray()));
+
+        ConnectorFacade connector = setupConnector(config);
+        connector.test();
+    }
 }

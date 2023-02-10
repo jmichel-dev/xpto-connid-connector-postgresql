b24dbfe4fbc7110031775ef96bb52d406a0428f7
Jean Michel <jean.michel@ebz.tec.br>
2023-02-10 12:03:00 -0300

    implemented user add

--- src/main/java/com/evolveum/polygon/connector/xpto/XptoConnector.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/XptoConnector.java
@@ -16,24 +16,35 @@
 
 package com.evolveum.polygon.connector.xpto;
 
+import com.evolveum.polygon.connector.xpto.entity.XptoUser;
+import com.evolveum.polygon.connector.xpto.service.XptoUserService;
+import com.evolveum.polygon.connector.xpto.utils.GenericExceptionHandler;
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.exceptions.ConnectionBrokenException;
 import org.identityconnectors.framework.common.exceptions.ConnectionFailedException;
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.OperationOptions;
+import org.identityconnectors.framework.common.objects.Uid;
 import org.identityconnectors.framework.spi.Configuration;
 import org.identityconnectors.framework.spi.Connector;
 import org.identityconnectors.framework.spi.ConnectorClass;
+import org.identityconnectors.framework.spi.operations.CreateOp;
 import org.identityconnectors.framework.spi.operations.TestOp;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Set;
 
 @ConnectorClass(displayNameKey = "xpto.connector.display", configurationClass = XptoConfiguration.class)
-public class XptoConnector implements Connector, TestOp {
+public class XptoConnector implements Connector, TestOp, CreateOp {
 
     private static final Log LOG = Log.getLog(XptoConnector.class);
 
     private XptoConfiguration configuration;
     private XptoConnection connection;
     private Connection databaseConnection;
+    private XptoUserService userService;
 
     @Override
     public Configuration getConfiguration() {
@@ -48,6 +59,7 @@ public class XptoConnector implements Connector, TestOp {
         try {
             connection.setUpConnection();
             databaseConnection = connection.getDatabaseConnection();
+            userService = connection.getXptoUserService();
         } catch (SQLException e) {
             throw new ConnectionFailedException(e);
         }
@@ -72,7 +84,32 @@ public class XptoConnector implements Connector, TestOp {
             databaseConnection.isValid(5);
             LOG.info("Test service execution finished successfully");
         } catch (SQLException e) {
-            throw new ConnectionFailedException(e);
+            throw new ConnectionBrokenException(e);
         }
+    }
+
+    @Override
+    public Uid create(ObjectClass objectClass, Set<Attribute> set, OperationOptions operationOptions) {
+        try {
+            String uid;
+
+            if (ObjectClass.ACCOUNT.equals(objectClass)) {
+                LOG.info("Started procedure for create user.");
+                LOG.info("Translating user...");
+                XptoUser user = XptoUser.translateUser(null, set);
+                LOG.info("User {0] translated successfully", user);
+                LOG.info("Creating user in the target...");
+                uid = userService.create(user);
+            } else {
+                throw new UnsupportedOperationException("Unknown object class " + objectClass);
+            }
+
+            LOG.ok("Created new object with the UID: {0}", uid);
+            return new Uid(uid);
+        } catch (Exception ex) {
+            new GenericExceptionHandler().handleGenericException(ex, "Couldn't create object " + objectClass + " with attributes " + set + ", reason: " + ex.getMessage());
+        }
+
+        return null;
     }
 }

dfd962f8cebabf577cad6f2f453b0ac8af703c6d
Jean Michel <jean.michel@ebz.tec.br>
2023-02-10 18:12:37 -0300

    translate user for midpoint connector object

--- src/main/java/com/evolveum/polygon/connector/xpto/entity/XptoUser.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/entity/XptoUser.java
@@ -1,15 +1,17 @@
 package com.evolveum.polygon.connector.xpto.entity;
 
+import com.evolveum.polygon.connector.xpto.service.XptoUserService;
 import com.evolveum.polygon.connector.xpto.utils.ManageAttributes;
 import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
-import org.identityconnectors.framework.common.objects.Attribute;
-import org.identityconnectors.framework.common.objects.OperationalAttributes;
-import org.identityconnectors.framework.common.objects.Uid;
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.objects.*;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
 
 public class XptoUser extends XptoObject {
+    private static final Log LOG = Log.getLog(XptoUser.class);
     private int user_id;
     private String first_name;
     private String last_name;
@@ -100,7 +102,39 @@ public class XptoUser extends XptoObject {
         user.setEmail(ManageAttributes.getAttributeValue(XptoAttributesConstants.XPTO_EMAIL, String.class, attributes));
         user.setPassword(ManageAttributes.getAttributeValue(OperationalAttributes.PASSWORD_NAME, String.class, attributes));
         user.setIs_active(ManageAttributes.getAttributeValue(OperationalAttributes.ENABLE_NAME, Boolean.class, attributes));
-
         return user;
+    }
+
+    public ConnectorObject translateToMidpointConnectorObject() {
+        ConnectorObjectBuilder builder = new ConnectorObjectBuilder();
+
+        builder.setObjectClass(ObjectClass.ACCOUNT);
+        addAttribute(builder, Uid.NAME, email);
+        addAttribute(builder, Name.NAME, email);
+        addAttribute(builder, XptoAttributesConstants.XPTO_EMAIL, email);
+        addAttribute(builder, XptoAttributesConstants.XPTO_FIRST_NAME, first_name);
+        addAttribute(builder, XptoAttributesConstants.XPTO_LAST_NAME, last_name);
+        addAttribute(builder, OperationalAttributes.ENABLE_NAME, is_active);
+
+        return builder.build();
+
+    }
+
+    private void addAttribute(ConnectorObjectBuilder builder, String name, Object value) {
+        if (value == null) {
+            return;
+        }
+
+        AttributeBuilder ab = new AttributeBuilder();
+        ab.setName(name);
+
+        if (value instanceof Collection) {
+            ab.addValue((Collection) value);
+        } else {
+            ab.addValue(value);
+        }
+
+        //LOG.ok("Attribute: " + name + " with value(s): " + value.toString() + " added to construction.");
+        builder.addAttribute(ab.build());
     }
 }

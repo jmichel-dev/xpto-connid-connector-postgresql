d188a335171cd36a04b9921125313cb04415bc88
Jean Michel <jean.michel@ebz.tec.br>
2023-02-10 11:59:06 -0300

    class to handle generic expection

--- src/main/java/com/evolveum/polygon/connector/xpto/utils/GenericExceptionHandler.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/utils/GenericExceptionHandler.java
@@ -1,2 +1,36 @@
-package com.evolveum.polygon.connector.xpto.utils;public class GenericExceptionHandler {
+package com.evolveum.polygon.connector.xpto.utils;
+
+import org.identityconnectors.framework.common.exceptions.*;
+import org.postgresql.util.PSQLException;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.NoRouteToHostException;
+import java.net.SocketTimeoutException;
+import java.sql.SQLException;
+
+public class GenericExceptionHandler {
+    public void handleGenericException(Exception ex, String message) {
+
+
+        if ( ex instanceof ConnectorException || ex instanceof UnsupportedOperationException || ex instanceof IllegalArgumentException) {
+            throw (RuntimeException) ex;
+        }
+
+        if (ex instanceof PSQLException) {
+            throw new ConnectionFailedException(message + ", Postgresql database exception occur, reason: " + ex.getMessage(), ex);
+        }
+
+        if (ex instanceof SQLException) {
+            throw new ConnectionFailedException(message + ", Postgresql database exception occur, reason: " + ex.getMessage(), ex);
+        }
+
+        if (ex instanceof IOException) {
+            if ((ex instanceof SocketTimeoutException || ex instanceof NoRouteToHostException)) {
+                throw new OperationTimeoutException(message + ", timeout occured, reason: " + ex.getMessage(), ex);
+            }
+
+            throw new ConnectorIOException(message + " IO exception occcured, reason: " + ex.getMessage(), ex);
+        }
+    }
 }

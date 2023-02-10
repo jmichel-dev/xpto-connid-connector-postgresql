42e2950c762d6394f712f0176f0e8e6437234546
Jean Michel <jean.michel@ebz.tec.br>
2023-02-10 11:59:58 -0300

    return attribute values based on midpoint attribute entry

--- src/main/java/com/evolveum/polygon/connector/xpto/utils/ManageAttributes.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/utils/ManageAttributes.java
@@ -1,2 +1,43 @@
-package com.evolveum.polygon.connector.xpto.utils;public class ManageAttributes {
+package com.evolveum.polygon.connector.xpto.utils;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.common.exceptions.InvalidAttributeValueException;
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.AttributeUtil;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public class ManageAttributes {
+    private static final Log LOG = Log.getLog(ManageAttributes.class);
+
+    public static <T> T getAttributeValue(String name, Class<T> type, Set<Attribute> attributes) {
+        LOG.ok("Processing attribute {0} of the type {1}", name, type.toString());
+
+        Attribute attr = AttributeUtil.find(name, attributes);
+
+        if (attr == null) {
+            return null;
+        }
+
+        if (String.class.equals(type)) {
+            return (T) AttributeUtil.getStringValue(attr);
+        } else if (Long.class.equals(type)) {
+            return (T) AttributeUtil.getLongValue(attr);
+        } else if (Integer.class.equals(type)) {
+            return (T) AttributeUtil.getIntegerValue(attr);
+        } else if (GuardedString.class.equals(type)) {
+            return (T) AttributeUtil.getGuardedStringValue(attr);
+        } else if (Boolean.class.equals(type)) {
+            return (T) AttributeUtil.getBooleanValue(attr);
+        } else if (List.class.equals(type)) {
+            return (T) attr.getValue();
+        } else if(Date.class.equals(type)) {
+            return (T) AttributeUtil.getDateValue(attr);
+        } else {
+            throw new InvalidAttributeValueException("Unknown value type " + type);
+        }
+    }
 }

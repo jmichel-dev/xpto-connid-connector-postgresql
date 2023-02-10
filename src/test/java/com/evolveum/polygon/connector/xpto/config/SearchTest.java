16132c7df85132114bfa124398b2b63e502181b4
Jean Michel <jean.michel@ebz.tec.br>
2023-02-10 17:12:51 -0300

    first test fot get user by email

--- src/test/java/com/evolveum/polygon/connector/xpto/config/SearchTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/SearchTest.java
@@ -1,2 +1,26 @@
-package com.evolveum.polygon.connector.xpto.config;public class SearchTest {
+package com.evolveum.polygon.connector.xpto.config;
+
+import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
+import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.AttributeBuilder;
+import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.filter.AndFilter;
+import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class SearchTest extends BaseConnectionTest{
+
+    @Test
+    public void shouldGetUserByEmail() throws Exception {
+        ConnectorFacade facade = setupConnector();
+
+        ListResultHandler handler = new ListResultHandler();
+        Attribute attr = AttributeBuilder.build(XptoAttributesConstants.XPTO_EMAIL, "jean.engenheirocomp@gmail.com");
+        EqualsFilter equalsFilter = new EqualsFilter(attr);
+        facade.search(ObjectClass.ACCOUNT, equalsFilter, handler, null);
+
+        AssertJUnit.assertTrue(handler.getObjects().size() > 0);
+    }
 }

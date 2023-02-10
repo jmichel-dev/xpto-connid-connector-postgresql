08a0c34102236888fec28e09d316773f63b8501a
Jean Michel <jean.michel@ebz.tec.br>
2023-02-10 12:01:32 -0300

    attributes name constants

--- src/main/java/com/evolveum/polygon/connector/xpto/utils/XptoAttributesConstants.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/utils/XptoAttributesConstants.java
@@ -1,2 +1,8 @@
-package com.evolveum.polygon.connector.xpto.utils;public interface XptoAttributesConstants {
+package com.evolveum.polygon.connector.xpto.utils;
+
+public interface XptoAttributesConstants {
+    String OBJECT_ATTR_ID = "id";
+    String XPTO_FIRST_NAME = "firstName";
+    String XPTO_LAST_NAME = "lastName";
+    String XPTO_EMAIL = "email";
 }

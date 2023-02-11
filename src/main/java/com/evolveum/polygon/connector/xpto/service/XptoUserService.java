0c1cc5cfce7d77d31e3a63c2c9aa506860969e7a
Jean Michel <jean.michel@ebz.tec.br>
2023-02-11 18:08:16 -0300

    service method to search users in database

--- src/main/java/com/evolveum/polygon/connector/xpto/service/XptoUserService.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/service/XptoUserService.java
@@ -1,12 +1,17 @@
 package com.evolveum.polygon.connector.xpto.service;
 
 import com.evolveum.polygon.connector.xpto.entity.XptoUser;
+import com.evolveum.polygon.connector.xpto.utils.StringAccessor;
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.common.security.GuardedString;
 import org.identityconnectors.framework.common.exceptions.InvalidAttributeValueException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XptoUserService {
     private static final Log LOG = Log.getLog(XptoUserService.class);
@@ -23,7 +28,10 @@ public class XptoUserService {
             stmt.setString(1, user.getFirst_name());
             stmt.setString(2, user.getLast_name());
             stmt.setString(3, user.getEmail());
-            stmt.setString(4, user.getPassword());
+            StringAccessor accessor = new StringAccessor();
+            GuardedString pwd = user.getPassword();
+            pwd.access(accessor);
+            stmt.setString(4, accessor.getValue());
             stmt.setBoolean(5, user.getIs_active());
             stmt.execute();
 
@@ -32,5 +40,34 @@ public class XptoUserService {
             LOG.error("Failed to create user in the database, reason: " + e.getMessage());
             throw new InvalidAttributeValueException("failed to create user, reason " + e.getMessage());
         }
+    }
+
+    public List<XptoUser> search(String filter) throws SQLException {
+        String sql = "SELECT * from users";
+
+        if (filter != null && !filter.isEmpty()) {
+            sql += " where " + filter;
+        }
+        List<XptoUser> users = new ArrayList<>();
+
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet result = stmt.executeQuery();
+
+        while (result.next()) {
+            String firstName = result.getString("first_name");
+            String lastName = result.getString("last_name");
+            String email = result.getString("email");
+            Boolean isActive = result.getBoolean("is_active");
+
+            XptoUser user = new XptoUser();
+            user.setEmail(email);
+            user.setLast_name(lastName);
+            user.setFirst_name(firstName);
+            user.setIs_active(isActive);
+
+            users.add(user);
+        }
+
+        return users;
     }
 }

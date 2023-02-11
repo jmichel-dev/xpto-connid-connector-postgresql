a5fba8c025289a738deaf97c677de2808be82829
Jean Michel <jean.michel@ebz.tec.br>
2023-02-11 18:36:42 -0300

    defined test for create users with the same email

--- src/test/java/com/evolveum/polygon/connector/xpto/config/AddTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/AddTest.java
@@ -4,6 +4,7 @@ import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
 import org.checkerframework.checker.units.qual.A;
 import org.identityconnectors.common.security.GuardedString;
 import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.exceptions.AlreadyExistsException;
 import org.identityconnectors.framework.common.objects.*;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
@@ -32,5 +33,20 @@ public class AddTest extends BaseConnectionTest {
         createdUid = facade.create(ObjectClass.ACCOUNT, set, null);
         AssertJUnit.assertNotNull(createdUid);
         AssertJUnit.assertEquals("jean.engenheirocomp@gmail.com", createdUid.getUidValue());
+    }
+
+    @Test(groups = "Add Test", expectedExceptions = AlreadyExistsException.class)
+    public void shouldNotCreateUserWithTheAnEmailThatAlreadyExists() throws Exception {
+        ConnectorFacade facade = setupConnector();
+
+        Set<Attribute> set = new HashSet<>();
+        set.add(AttributeBuilder.build(Name.NAME, "jean.engenheirocomp@gmail.com"));
+        set.add(AttributeBuilder.build(XptoAttributesConstants.XPTO_FIRST_NAME, "Jean"));
+        set.add(AttributeBuilder.build(XptoAttributesConstants.XPTO_LAST_NAME, "Santos"));
+        set.add(AttributeBuilder.build(XptoAttributesConstants.XPTO_EMAIL, "jean.engenheirocomp@gmail.com"));
+        set.add(AttributeBuilder.build(OperationalAttributes.ENABLE_NAME, true));
+        set.add(AttributeBuilder.build(OperationalAttributes.PASSWORD_NAME, new GuardedString(PASSWORD.toCharArray())));
+
+        createdUid = facade.create(ObjectClass.ACCOUNT, set, null);
     }
 }

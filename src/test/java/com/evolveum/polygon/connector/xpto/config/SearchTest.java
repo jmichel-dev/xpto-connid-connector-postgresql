4b1e53cc830994be9b532f56a67e41a03b6c1c57
Jean Michel <jean.michel@ebz.tec.br>
2023-02-11 19:07:08 -0300

    defined assert equals for user email returned

--- src/test/java/com/evolveum/polygon/connector/xpto/config/SearchTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/SearchTest.java
@@ -1,18 +1,19 @@
 package com.evolveum.polygon.connector.xpto.config;
 
+import com.evolveum.polygon.connector.xpto.entity.XptoUser;
 import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
 import org.identityconnectors.framework.api.ConnectorFacade;
-import org.identityconnectors.framework.common.objects.Attribute;
-import org.identityconnectors.framework.common.objects.AttributeBuilder;
-import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.*;
 import org.identityconnectors.framework.common.objects.filter.AndFilter;
 import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class SearchTest extends BaseConnectionTest{
 
-    @Test
+    @Test(groups = "SearchTestOP")
     public void shouldGetUserByEmail() throws Exception {
         ConnectorFacade facade = setupConnector();
 
@@ -22,5 +23,11 @@ public class SearchTest extends BaseConnectionTest{
         facade.search(ObjectClass.ACCOUNT, equalsFilter, handler, null);
 
         AssertJUnit.assertTrue(handler.getObjects().size() > 0);
+
+        List<ConnectorObject> objects = handler.getObjects();
+        ConnectorObject object = objects.get(0);
+        String email = AttributeUtil.getStringValue(object.getAttributeByName("email"));
+
+        AssertJUnit.assertEquals("jean.engenheirocomp@gmail.com", email);
     }
 }

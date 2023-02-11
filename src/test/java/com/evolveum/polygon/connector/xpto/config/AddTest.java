5e8b219e29fc24de9c047280ea121334639846c9
Jean Michel <jean.michel@ebz.tec.br>
2023-02-11 18:08:44 -0300

    test for create succesfully a normal user

--- src/test/java/com/evolveum/polygon/connector/xpto/config/AddTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/AddTest.java
@@ -1,2 +1,36 @@
-package com.evolveum.polygon.connector.xpto.config;public class AddTest {
+package com.evolveum.polygon.connector.xpto.config;
+
+import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
+import org.checkerframework.checker.units.qual.A;
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.objects.*;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.Attributes;
+
+public class AddTest extends BaseConnectionTest {
+
+    private Uid createdUid;
+    private static final String PASSWORD = "smartway";
+
+    @Test(groups = "Add Test")
+    public void shouldCreateAnUserSuccessfully() throws Exception {
+        ConnectorFacade facade = setupConnector();
+
+        Set<Attribute> set = new HashSet<>();
+        set.add(AttributeBuilder.build(Name.NAME, "jean.engenheirocomp@gmail.com"));
+        set.add(AttributeBuilder.build(XptoAttributesConstants.XPTO_FIRST_NAME, "Jean"));
+        set.add(AttributeBuilder.build(XptoAttributesConstants.XPTO_LAST_NAME, "Santos"));
+        set.add(AttributeBuilder.build(XptoAttributesConstants.XPTO_EMAIL, "jean.engenheirocomp@gmail.com"));
+        set.add(AttributeBuilder.build(OperationalAttributes.ENABLE_NAME, true));
+        set.add(AttributeBuilder.build(OperationalAttributes.PASSWORD_NAME, new GuardedString(PASSWORD.toCharArray())));
+
+        createdUid = facade.create(ObjectClass.ACCOUNT, set, null);
+        AssertJUnit.assertNotNull(createdUid);
+        AssertJUnit.assertEquals("jean.engenheirocomp@gmail.com", createdUid.getUidValue());
+    }
 }

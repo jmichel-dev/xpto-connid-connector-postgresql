e4f10382289962c8a384b19cb92e28d06d30cc65
Jean Michel <jean.michel@ebz.tec.br>
2023-02-15 11:58:13 -0300

    implemented delete test

--- src/test/java/com/evolveum/polygon/connector/xpto/config/DeleteOpTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/DeleteOpTest.java
@@ -1,2 +1,27 @@
-package com.evolveum.polygon.connector.xpto.config;public class DeleteOpTest {
+package com.evolveum.polygon.connector.xpto.config;
+
+import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
+import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DeleteOpTest extends BaseConnectionTest {
+    @Test(groups = "DeleteOpTest")
+    public void shouldRemoveAnUserWithAnExistentRecord() throws Exception {
+        String email = "jean.engenheirocomp@gmail.com";
+        ConnectorFacade facade = setupConnector();
+
+        facade.delete(ObjectClass.ACCOUNT, new Uid(email), null);
+
+        ListResultHandler handler = new ListResultHandler();
+        Attribute attr = AttributeBuilder.build(XptoAttributesConstants.XPTO_EMAIL, "jean.engenheirocomp@gmail.com");
+        EqualsFilter equalsFilter = new EqualsFilter(attr);
+        facade.search(ObjectClass.ACCOUNT, equalsFilter, handler, null);
+
+        AssertJUnit.assertEquals(0, handler.getObjects().size());
+    }
 }

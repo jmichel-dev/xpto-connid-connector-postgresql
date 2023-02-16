8a9caa7e891ee5cfbf4e898095e49147908f9c7b
Jean Michel <jean.michel@ebz.tec.br>
2023-02-16 08:59:57 -0300

    handling update users

--- src/main/java/com/evolveum/polygon/connector/xpto/XptoConnector.java
+++ src/main/java/com/evolveum/polygon/connector/xpto/XptoConnector.java
@@ -16,33 +16,35 @@
 
 package com.evolveum.polygon.connector.xpto;
 
+import com.evolveum.polygon.connector.xpto.entity.XptoObject;
 import com.evolveum.polygon.connector.xpto.entity.XptoUser;
 import com.evolveum.polygon.connector.xpto.service.XptoUserService;
 import com.evolveum.polygon.connector.xpto.utils.FilterHandler;
 import com.evolveum.polygon.connector.xpto.utils.GenericExceptionHandler;
+import com.evolveum.polygon.connector.xpto.utils.ProcessingAttributesDelta;
 import org.identityconnectors.common.CollectionUtil;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.exceptions.ConnectionBrokenException;
 import org.identityconnectors.framework.common.exceptions.ConnectionFailedException;
+import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.identityconnectors.framework.common.exceptions.UnknownUidException;
 import org.identityconnectors.framework.common.objects.*;
 import org.identityconnectors.framework.common.objects.filter.Filter;
 import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
 import org.identityconnectors.framework.spi.Configuration;
 import org.identityconnectors.framework.spi.Connector;
 import org.identityconnectors.framework.spi.ConnectorClass;
-import org.identityconnectors.framework.spi.operations.CreateOp;
-import org.identityconnectors.framework.spi.operations.DeleteOp;
-import org.identityconnectors.framework.spi.operations.SearchOp;
-import org.identityconnectors.framework.spi.operations.TestOp;
+import org.identityconnectors.framework.spi.operations.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @ConnectorClass(displayNameKey = "xpto.connector.display", configurationClass = XptoConfiguration.class)
-public class XptoConnector implements Connector, TestOp, CreateOp, SearchOp<Filter>, DeleteOp {
+public class XptoConnector implements Connector, TestOp, CreateOp, SearchOp<Filter>, DeleteOp, UpdateDeltaOp {
 
     private static final Log LOG = Log.getLog(XptoConnector.class);
 
@@ -180,5 +182,137 @@ public class XptoConnector implements Connector, TestOp, CreateOp, SearchOp<Filt
         } catch (Exception e) {
             GenericExceptionHandler.handleGenericException(e, "Couldn't delete " + objectClass + " with the uid " + uid + ", reason: " + e.getMessage());
         }
+    }
+
+    @Override
+    public Set<AttributeDelta> updateDelta(ObjectClass objectClass, Uid uid, Set<AttributeDelta> set, OperationOptions operationOptions) {
+        Uid newUid = null;
+        ProcessingAttributesDelta deltas = new ProcessingAttributesDelta(set);
+        Set<Attribute> attrsToReplace = deltas.getAttrsToReplace();
+        Set<Attribute> attrsToRemove = deltas.getAttrsToRemove();
+        Set<Attribute> attrsToAdd = deltas.getAttrsToAdd();
+
+        try {
+            ConnectorObject old = getOldObject(objectClass, uid);
+
+            LOG.ok("fetched the object with the uid {0} from the resource for delta update", uid.getUidValue());
+
+            Set<Attribute> processedAttrs = new HashSet<>(old.getAttributes());
+
+            if (!attrsToReplace.isEmpty()) {
+                LOG.ok("Processing through replace set of attributes in the update attribute delta op");
+
+                attrsToReplace.forEach(newAttr -> {
+                    Attribute oldAttr = AttributeUtil.find(newAttr.getName(), processedAttrs);
+                    if (oldAttr != null) {
+                        processedAttrs.remove(oldAttr);
+                    }
+                    processedAttrs.add(newAttr);
+                });
+            }
+
+            if (!attrsToAdd.isEmpty()) {
+                LOG.ok("Processing through ADD set of attributes in the update attribute delta op");
+
+                attrsToAdd.forEach(newAttr -> {
+                    Attribute oldAttr = AttributeUtil.find(newAttr.getName(), processedAttrs);
+
+                    if (oldAttr != null) {
+                        List values = new ArrayList();
+                        if (oldAttr.getValue() != null) {
+                            values.addAll(oldAttr.getValue());
+                        }
+
+                        values.addAll(newAttr.getValue());
+                        processedAttrs.remove(oldAttr);
+                        processedAttrs.add(AttributeBuilder.build(oldAttr.getName(), values));
+                    } else {
+                        processedAttrs.add(newAttr);
+                    }
+                });
+            }
+
+            if (!attrsToRemove.isEmpty()) {
+                LOG.ok("Processing through DELETE set of attributes in the update attribute delta op.");
+
+                attrsToRemove.forEach(removeAttr -> {
+                    Attribute oldAttr = AttributeUtil.find(removeAttr.getName(), processedAttrs);
+
+                    if (oldAttr != null) {
+                        List values = new ArrayList();
+
+                        if (oldAttr.getValue() != null) {
+                            values.addAll(oldAttr.getValue());
+                        }
+
+                        values.removeAll(removeAttr.getValue());
+                        processedAttrs.remove(oldAttr);
+                        processedAttrs.add(AttributeBuilder.build(oldAttr.getName(), values));
+                    }
+                });
+            }
+
+            newUid = updateObject(objectClass, uid, processedAttrs);
+
+        } catch (Exception e) {
+            GenericExceptionHandler.handleGenericException(e, "Couldn't modify attribute values from object " + objectClass +
+                    " with uid " + uid + ", reason: " + e.getMessage());
+        }
+
+        Set<AttributeDelta> returnDelta = new HashSet<>();
+
+        if (newUid == null || newUid != uid) {
+            AttributeDelta newUidAttributeDelta = AttributeDeltaBuilder.build(Uid.NAME, uid.getUidValue());
+            returnDelta.add(newUidAttributeDelta);
+        }
+
+        return returnDelta;
+    }
+
+    private ConnectorObject getOldObject(ObjectClass objectClass, Uid uid) {
+        XptoUser object;
+
+        try {
+            if (ObjectClass.ACCOUNT.equals(objectClass)) {
+                object = userService.searchById(uid.getUidValue());
+            } else {
+                throw new UnsupportedOperationException("Unknown object class " + objectClass);
+            }
+
+            if (object == null) {
+                throw new UnknownUidException("Couldn't find object " + objectClass + " with uid " + uid.getUidValue());
+            }
+
+            //LOG.info("old object found " + object.toString());
+
+            return object.translateToMidpointConnectorObject();
+        } catch (Exception e) {
+            GenericExceptionHandler.handleGenericException(e, "Couldn't search object by uid " + uid.getUidValue() + ", reason: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    private Uid updateObject(ObjectClass objectClass, Uid uid, Set<Attribute> attributes) {
+        String uidString;
+        try {
+            if (ObjectClass.ACCOUNT.equals(objectClass)) {
+                XptoUser user = XptoUser.translateUser(uid, attributes);
+                LOG.info("New user processed {0}", user);
+                uidString = userService.updateUser(user);
+            } else {
+                throw new UnsupportedOperationException("Unknown object class " + objectClass);
+            }
+            if (uidString != null) {
+                return new Uid(uidString);
+            } else {
+                throw new ConnectorException("Unexpected exception occurred. No uid returned by resource after update" +
+                        " operation execution for the object with the uid: " + uid + ".");
+            }
+        } catch (Exception e) {
+            GenericExceptionHandler.handleGenericException(e, "Couldn't update " + objectClass + " with the uid " + uid + ", reason: " + e.getMessage());
+        }
+
+        return null;
     }
 }

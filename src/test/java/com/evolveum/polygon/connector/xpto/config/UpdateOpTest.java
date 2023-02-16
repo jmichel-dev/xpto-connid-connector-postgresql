ee34223d377a76ec392e8566d702de1d87e10828
Jean Michel <jean.michel@ebz.tec.br>
2023-02-16 09:02:27 -0300

    test for update first name user

--- src/test/java/com/evolveum/polygon/connector/xpto/config/UpdateOpTest.java
+++ src/test/java/com/evolveum/polygon/connector/xpto/config/UpdateOpTest.java
@@ -1,2 +1,35 @@
-package com.evolveum.polygon.connector.xpto.config;public class UpdateTestOp {
+package com.evolveum.polygon.connector.xpto.config;
+
+import com.evolveum.polygon.connector.xpto.utils.XptoAttributesConstants;
+import org.identityconnectors.framework.api.ConnectorFacade;
+import org.identityconnectors.framework.common.objects.*;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UpdateOpTest extends BaseConnectionTest {
+
+    private static final String UID = "jean.engenheirocomp@gmail.com";
+
+    @Test(groups = "UpdateTestOp")
+    public void shouldUpdateUserFirstName() throws Exception {
+        ConnectorFacade facade = setupConnector();
+
+        Set<AttributeDelta> attributeDeltas = new HashSet<>();
+
+        AttributeDeltaBuilder builder = new AttributeDeltaBuilder();
+        builder.setName(XptoAttributesConstants.XPTO_FIRST_NAME);
+        builder.addValueToReplace("Joao");
+
+        attributeDeltas.add(builder.build());
+
+        facade.updateDelta(ObjectClass.ACCOUNT, new Uid(UID), attributeDeltas, null);
+
+        ConnectorObject object = facade.getObject(ObjectClass.ACCOUNT, new Uid(UID), null);
+        Attribute attr = AttributeUtil.find(XptoAttributesConstants.XPTO_FIRST_NAME, object.getAttributes());
+
+        AssertJUnit.assertEquals("Joao", attr.getValue().get(0));
+    }
 }
